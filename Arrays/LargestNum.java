0b947df9f963837fa3e3a4bdd16b049ece89386f
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-05-30 21:39:47 +0530

    updated version

--- Arrays/LargestNum.java
+++ Arrays/LargestNum.java
@@ -1,20 +1,24 @@
 public class LargestNum {
 
     // Function to find the largest number in an array
-    public static int findLargest(int numbers[], int num){
-        
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE; 
+        int smallest = Integer.MAX_VALUE; 
         for(int i=0; i<numbers.length; i++){
-            if(numbers[i] > num){
-                num = numbers[i];
+            if(largest < numbers[i]){
+                largest = numbers[i];
             }
-            
+            if(smallest > numbers[i]){
+                smallest = numbers[i] ;
+            }
         }
-        return num;
+        System.out.println("Smallest number is : " + smallest);
+        return largest;
     }
 
     public static void main(String args[]){
         int numbers[] = {23, 43, 234, 45, 67, 89, 90, 12, 34};
-        int largest = findLargest(numbers,0);
+        int largest = getLargest(numbers);
         System.out.println("Largest number in the array is: " + largest);
     }
 }
