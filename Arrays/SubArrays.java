78c3cf2531d6e48980e29380617f081be9adf342
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-05-31 20:07:43 +0530

    SubArrays

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -1,21 +1,25 @@
 public class SubArrays {
     public static int getSubArrays(int numbers[]){
+        int ts=0;
         for(int i=0;i<numbers.length;i++){
             int start =i;
             for(int j=i;j<numbers.length;j++){
                 int end = j;
                 for(int k=start;k<=end;k++){
                     System.out.print(numbers[k]+" ");
+        
                 }
                 System.out.println();
             }
+            ts++;
             System.out.println();
         }
+        System.out.print("total subarrays : " + ts);
         return 0;
     }
     public static void main(String args[]){
         int numbers[]={2,4,6,8,10};
-        System.out.println("All subarrays in the array:");
+        System.out.println("All subarrays in the array : ");
         getSubArrays(numbers);
     }
 }
