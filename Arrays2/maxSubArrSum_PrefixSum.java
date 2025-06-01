65d442833bef96d0232e300dc353bd16301e45bf
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-01 19:08:38 +0530

    PrefixSum

---
+++ Arrays2/maxSubArrSum_PrefixSum.java
@@ -0,0 +1,28 @@
+public class maxSubArrSum_PrefixSum {
+    public static int maxSubarraySum(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n];
+        prefix[0] = arr[0];
+
+        // prefix sum array
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int minPrefix = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxSum = Math.max(maxSum, prefix[i] - minPrefix);
+            minPrefix = Math.min(minPrefix, prefix[i]);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = maxSubarraySum(arr);
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
