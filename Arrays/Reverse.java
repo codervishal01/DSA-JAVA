bbe88dc07585c870610c621a766d284a8cb60571
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-03 16:25:04 +0530

    TrappedWater

--- Arrays/Reverse.java
+++ Arrays/Reverse.java
@@ -1,9 +1,9 @@
 public class Reverse {
-    public static void reverse(int numbers[]){
-        int first=0,last=numbers.length-1;
+    public static void reverse(int numbers[]) {
+        int first = 0, last = numbers.length - 1;
 
-        while(first<last){
-            int temp= numbers[last];
+        while (first < last) {
+            int temp = numbers[last];
             numbers[last] = numbers[first];
             numbers[first] = temp;
 
@@ -11,11 +11,12 @@ public class Reverse {
             last--;
         }
     }
-    public static void main(String args[]){
-        int numbers[] = {2,6,8,10,12,14};
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 6, 8, 10, 12, 14 };
         reverse(numbers);
         System.out.print("Reversed array: ");
-        for(int i=0; i<numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
     }
--- Arrays2/trappedWater.java
+++ Arrays2/trappedWater.java
@@ -1,35 +1,36 @@
 public class trappedWater {
 
-    public static int calculateTrappedWater(int height[]) {
+    public static int calTrappedWater(int height[]) {
         int n = height.length;
-        if (n == 0) return 0;
-
-        // Calculate left max boundary
+        // Calculate left max
         int leftMax[] = new int[n];
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
-        // Calculate right max boundary
+        // Calculate right max
         int rightMax[] = new int[n];
         rightMax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
-
-        // Calculate trapped water
-        int trappedWater = 0;
+        // loop
+        int totalTrappedWater = 0;
         for (int i = 0; i < n; i++) {
+            // Calculate min of left max and right max
             int waterLevel = Math.min(leftMax[i], rightMax[i]);
-            trappedWater += waterLevel - height[i];
-        }
 
-        return trappedWater;
+            // Calculate Trapped   Water = min(leftMax, rightMax) - height[i]
+            int trappedWater = waterLevel - height[i];
+            totalTrappedWater += trappedWater;
+        }
+        return totalTrappedWater;
+
     }
 
     public static void main(String args[]) {
         int height[] = { 4, 2, 0, 6, 3, 2, 5 };
-        System.out.println("Trapped Water: " + calculateTrappedWater(height));
+        System.out.println("Total Trapped Water: " + calTrappedWater(height));
     }
 }
