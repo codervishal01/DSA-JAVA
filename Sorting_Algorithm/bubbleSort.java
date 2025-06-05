bf105bc2421c049530bf228a8a17aea283e563d7
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-05 10:05:35 +0530

    Bubble Sorting

--- Sorting_Algorithm/bubbleSort.java
+++ Sorting_Algorithm/bubbleSort.java
@@ -1,5 +1,29 @@
-package Sorting_Algorithm;
+import java.util.*;
 
 public class bubbleSort {
-    
+
+    public static void bubbleSorting(int nums[]) {
+        int n = nums.length;
+        for (int turns = 0; turns < n - 1; turns++) {
+            for (int j = 0; j < n - 1 - turns ; j++) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printnums(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int nums[] = { 5, 3, 8, 4, 2 };
+        bubbleSorting(nums);
+        printnums(nums);
+    }
 }
