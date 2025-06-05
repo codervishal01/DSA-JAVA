e7129e6eabb19c7cc92e4d1cd5ffe3bae381511b
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-05 19:55:28 +0530

    Insertion Sorting

---
+++ Sorting_Algorithm/InsertionSort.java
@@ -0,0 +1,36 @@
+public class InsertionSort {
+
+    public static void insertionSorting(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // Place the current element at its correct position
+            arr[j + 1] = current;
+        }
+    }
+
+    //  print the array
+    public static void printArray(int arr[]) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    
+    public static void main(String[] args) {
+        int arr[] = {9, 5, 1, 4, 3};
+    
+        insertionSorting(arr);
+        System.out.println("After sorting:");
+        printArray(arr);
+    }
+}
