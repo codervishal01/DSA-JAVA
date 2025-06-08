354f50c623c4e2555644848e28119448e1e19292
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-08 17:23:24 +0530

    subString

---
+++ Strings/substring.java
@@ -0,0 +1,26 @@
+public class substring {
+
+    public static String subString(String str,int si,int ei){
+    //     for(int i=0;i<str.length();i++){
+    //         for(int j=0;j<str.length();j++){
+    //             for(int k=i;k<=j;k++){
+    //                 System.out.print(str.charAt(k));
+    //             }
+    //             System.out.println();
+    //         }
+            
+    //     }
+    // }
+    String substr="";
+    for(int i=si;i<ei;i++){
+        substr +=str.charAt(i);
+        
+    }
+    return substr;
+}
+    public static void main(String args[]){
+        String str="HelloWorld";
+        System.out.print(subString(str,0,5));
+
+    }
+}
