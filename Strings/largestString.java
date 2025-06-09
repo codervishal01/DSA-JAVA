ee0518922695c954d06bd703fd8e1822a2485f05
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-09 20:15:38 +0530

    get largest String

---
+++ Strings/largestString.java
@@ -0,0 +1,24 @@
+public class LargestStringFinder {
+    
+    public static void printLargestString(String[] arr) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("Array is empty or null");
+            return;
+        }
+
+        String largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i].compareTo(largest) > 0) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest string: " + largest);
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"apple", "banana", "zebra", "mango", "grape"};
+        printLargestString(strings);
+    }
+}
