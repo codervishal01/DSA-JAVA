24fc195b155e7ea25c3a37da23385e6fdddd25d5
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-10 11:41:56 +0530

    Bit Manupulation

---
+++ Bit-Manipulation/bitwise.java
@@ -0,0 +1,5 @@
+package Bit-Manipulation;
+
+public class bitwise {
+    
+}
--- Practise.java
+++ Practise.java
@@ -1,17 +1,23 @@
-public class Practise {
-    public static void main(String[] args) {
-        
-    for(int i=1;i<=4;i++){
-        for(int j=4;j>=i; j--){
-           if( j<=(j-i)+1){
-                System.out.print("*");
-            } else {
-                System.out.print(" ");
-           }
+class Practise {
+    public int majorityElement(int[] nums,int key) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==key){
+                count++;
+            }
         }
-        System.out.println();
+        if(count>nums.length/2){
+            return key;
+        }
+        // Return -1 if key is not the majority element
+        return -1;
     }
-    
-    
+    public static void main(String args[]){
+        int nums[]={2,2,1,1,1,2,2};
+        int key=2;
+        Practise obj = new Practise();
+        int result = obj.majorityElement(nums, key);
+        System.out.print(result);
+
     }
 }
