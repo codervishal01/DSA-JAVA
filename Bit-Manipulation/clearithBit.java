aacee076b64efc324f51cfd7ffe3c6bd5f17a859
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-06-11 22:24:58 +0530

    clearithBit

---
+++ Bit-Manipulation/clearithBit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class clearithBit {
+    
+    public static int clearithBit(int ith, int num) {
+        int bitMask = ~(1 << ith);
+        return num & bitMask;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int ith = sc.nextInt();
+        int num = sc.nextInt();
+        System.out.print(clearithBit(ith, num));
+    }
+}
+
+
+
