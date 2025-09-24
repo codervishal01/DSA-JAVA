8b8ac503f8cd00f42f1919bd11330f114809cec4
codervishal01 <madhusudan_24a12res349@iitp.ac.in>
2025-09-24 16:23:49 +0530

    Constructor

--- Oops/Constructor.java
+++ Oops/Constructor.java
@@ -1,7 +1,9 @@
 public class Constructor {
     public static void main(String args[]){
         Student s1= new Student("Vishal");
+        Student s2 = new Student("20");
         System.out.print(s1.name);
+        System.out.print(s2.age);
     }
 }
 
@@ -9,7 +11,15 @@ class Student{
     String name;
     int age;
 
-    Student(String name){
+    Student(String name){    //parameterized constructor
         this.name = name;
+    }
+
+    Student(){  //non parameterized constructor
+        System.out.println("this is non parameterized constructor,,,");
+    }
+
+    Student(int age){    //parameterized constructor
+        this.age = age;
     }
 }
